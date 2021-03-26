be5b16d00e783f3bda988c218e862c5aabde0e90
lmaciel <lmaciel@cpqd.com.br>
2021-03-26 10:19:30 -0300

    Aprendizagem React - Funcionalidades báscias dos componentes

--- curso-react/fundamentos-react/src/App.jsx
+++ curso-react/fundamentos-react/src/App.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 
+import Card from "./components/layout/Card";
 import Primeiro from "./components/basic/Primeiro";
 import ComParametros from "./components/basic/ComParametros";
 import Fragmento from "./components/basic/Fragmento";
 import Aleatorio from "./components/basic/Aleatorio";
 
+
 export default _ =>
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio min={1} max={60} />
-        <Fragmento /> 
-        <ComParametros
-            titulo="Situação do Aluno"
-            aluno="Pedro Silva"
-            nota={9.3}
-        />
-        <Primeiro />
+        <Card titulo="#4 Desafio Aleatório">
+            <Aleatorio min={1} max={60} />
+        </Card>
+
+        <Card titulo="#3 Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#2 Com Paramentros">
+            <ComParametros
+                titulo="Situação do Aluno"
+                aluno="Pedro Silva"
+                nota={9.3}
+            />
+        </Card>
+
+        <Card titulo="#1 Primeiro Componente">
+            <Primeiro />
+        </Card>
     </div>
